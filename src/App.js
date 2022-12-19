c5486ba82335c37eca268cccb26f9334dde25f6f
Ikchhit Kumar Pandey <ikchhit10@gmail.com>
2022-12-19 22:35:49 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -31,7 +31,8 @@ function App() {
       <Alert alert={alert}/>
         <div className='container'>
         <Routes>
-          <Route exact path="/" element={<Home showAlert={showAlert}/>} />
+          {/* <Route exact path="/" element={<Home showAlert={showAlert}/>} /> */}
+          <Route exact path="/" element={<Login showAlert={showAlert}/>} />
           <Route exact path="/about" element={<About/>} />
           <Route exact path="/login" element={<Login showAlert={showAlert}/>} />
           <Route exact path="/signup" element={<Signup showAlert={showAlert}/>} />
