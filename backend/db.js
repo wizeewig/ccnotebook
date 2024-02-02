5e7f63fb5626b3dca1bfe95ff6c10da2d8e64982
wizeewig <ikchhit10@gmail.com>
2024-02-02 21:52:41 +0530

    New Commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-const mongoURI= "mongodb://localhost:27017/CC-Notebook";
+const mongoURI=   "mongodb+srv://ikchhit:QHJ6XJPXfJE7xsug@cluster0.gdclbha.mongodb.net/?retryWrites=true&w=majority";
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURI, ()=>{
