4ada36f84168fcbaba40402f847f08482b57dcd3
Nanhe Kumar <amritpandey8076@gmail.com>
2024-02-02 21:22:54 +0530

    Revert "New commit"
    This reverts commit 328d17aeaca3027871f7e766528a7cb482474a93.

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose= require('mongoose');
-const mongoURI= "mongodb+srv://ikchhit:QHJ6XJPXfJE7xsug@cluster0.gdclbha.mongodb.net/?retryWrites=true&w=majority";
+const mongoURI= "mongodb://localhost:27017/CC-Notebook";
 
 const connectToMongo =()=>{
     mongoose.connect(mongoURI, ()=>{
